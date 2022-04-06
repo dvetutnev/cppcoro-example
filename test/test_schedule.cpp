4254929c96c6e269609484d9984ede2e45630e3b
Dmitriy Vetutnev <d.vetutnev@gmail.com>
2022-04-06 18:00:21 +0300

    Replace class Scheduler to free-function schedule

--- test/test_schedule.cpp
+++ test/test_schedule.cpp
@@ -1,41 +1,50 @@
-#include "scheduler_asio.h"
+#include "schedule_asio.h"
 
-#include <boost/asio/experimental/awaitable_operators.hpp>
+#include <boost/asio/io_context.hpp>
 #include <boost/asio/detached.hpp>
+#include <boost/asio/experimental/awaitable_operators.hpp>
 
 #include <gtest/gtest.h>
 
 
-TEST(BoostASIO, Scheduler) {
-    boost::asio::io_context ioContext;
-    Scheduler scheduler{ioContext};
-
+TEST(schedule_asio, _) {
     bool reachedPointA = false;
     bool reachedPointB = false;
 
+
     auto process = [&]() -> boost::asio::awaitable<void> {
             reachedPointA = true;
-            co_await scheduler.schedule();
+
+            boost::asio::any_io_executor executor = co_await boost::asio::this_coro::executor;
+            co_await schedule(executor);
+
             reachedPointB = true;
+
             co_return;
     };
+
 
     auto check = [&]() -> boost::asio::awaitable<void> {
             EXPECT_TRUE(reachedPointA);
             EXPECT_FALSE(reachedPointB);
+
             co_return;
     };
+
 
     auto task = [&]() -> boost::asio::awaitable<void> {
             using namespace boost::asio::experimental::awaitable_operators;
 
             co_await(process() && check());
+
             co_return;
     };
 
-    boost::asio::co_spawn(ioContext, task(), boost::asio::detached);
 
+    boost::asio::io_context ioContext;
+    boost::asio::co_spawn(ioContext, task(), boost::asio::detached);
     ioContext.run();
+
 
     EXPECT_TRUE(reachedPointA);
     EXPECT_TRUE(reachedPointB);
